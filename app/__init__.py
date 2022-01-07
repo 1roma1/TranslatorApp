fcaf1eef747c0809ff8e3926c8c85aa03ecbc4ce
Roman Sayko <1romasayko1@gmail.com>
2022-01-07 18:11:37 +0300

    add wsgi

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,12 @@
-import os 
-
 from flask import Flask
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-    )
+def create_app():
+    app = Flask(__name__, instance_relative_config=False)
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
-    
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-       
-
-    from . import view
-    app.register_blueprint(view.bp)
-
+    with app.app_context():
+        from . import view
+        app.register_blueprint(view.bp)
+        
     return app
+    
+app = create_app()
