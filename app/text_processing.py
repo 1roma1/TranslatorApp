8c7ed321cff27b02d4dfd9b3b6854d5793c3bf50
Roman Sayko <1romasayko1@gmail.com>
2022-01-23 17:11:31 +0300

    add readme and change preprocessing

--- app/text_processing.py
+++ app/text_processing.py
@@ -4,12 +4,14 @@ import re
 def preprocess_input(input_text):
     input_text = re.sub(r'\s([?.!,"](?:\s|$))', r'\1', input_text)
     split_text = re.split('([.?!])', input_text)
+
+    print(split_text)
     if split_text[-1] != '':
-        split_text.append('')
+        split_text.append('.')
     input_text = []
 
     for i in range(0, len(split_text), 2):
-        if split_text[i] != '':
+        if split_text[i].strip() != '':
             input_text.append((split_text[i]+split_text[i+1]).lstrip())
 
     return input_text
@@ -17,6 +19,7 @@ def preprocess_input(input_text):
 def preprocess_output(output_text):
     output_text = re.sub(r'\s([?.!,"](?:\s|$))', r'\1', output_text)
     split_text = re.split('([.?!])\s', output_text)
+    print(split_text)
     output_text = ""
     for i in range(0, len(split_text), 2):
         if split_text[i] != '':
--- readme.md
+++ readme.md
@@ -1,4 +1,8 @@
-# **Ru Eng Translation Site**
+# **Ru Eng Translation Web App**
+
+Website Deployed: [Link](https://ru-eng-translator.herokuapp.com/)
+
+Data Science Project: [Link](https://github.com/1roma1/TranslationModel)
 
 ## **Illustrations**
 ![Image](assets/img.png)
